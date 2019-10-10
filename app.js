de88dbae3c441fa1448cbf4199a578f0d62ce447
ernesthenry <henry38ernest@gmail.com>
2019-10-10 12:31:56 -0400

    Add promises.all() method to handle multiple requests

---
+++ app.js
@@ -0,0 +1,64 @@
+// create a reusable function
+function fetchData(url){
+    return fetch(url)
+    .then(checkStatus)
+    .then(res => res.json)
+    .catch(error => console.log('Looks like there was a problem', error))
+}
+
+
+function checkStatus(response){
+    if(response.ok){
+        return Promise.resolve(response);
+    }
+    else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+Promise.all(
+    [
+        fetchData('https://dog.ceo/api/breeds/image/random'),
+        fetchData('https://dog.ceo/api/breeds/list/all')
+    ]
+).then(data => {
+    const breedList = data[0].message;
+    const randomImage = data[1].message;
+
+    generateOptions(breedList);
+    generateImage(randomImage);
+});
+
+
+function generateOptions(data){
+    const options = data.map(item => `
+    <option value='${item}'>${item}</option>
+    `).join('')
+}
+
+
+function generateImage(data){
+    html = `
+    <img src='${data}' alt>
+    <p>Click to view images of ${select.value}s</p>`
+    ;
+    card.innerHTML = html;
+
+}
+
+function fetchBreedImage(){
+    const breed = select.value;
+    const img = card.querySelector('img');
+    const p =  card.querySelector('p');
+
+    fetchData(`https://dog.ceo/api/${breed}/hound/images`)
+    .then(data => {
+        img.src = data.message;
+        img.alt = breed;
+        p.textContent = `Click to view more ${breed}s`
+    } )
+}
+
+// event listening
+
+select.addEventListener('change', fetchBreedImage)
+select.addEventListener('click', fetchBreedImage)
